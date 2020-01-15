0df0ab822251b17d592c0d11e0eb14c806ac5b1d
ElTangoBandito <Volfurious@gmail.com>
2020-01-15 02:08:33 -0800

    tested skit data structure, EVERYTHING WERKS

--- Ten Hour Shift/Assets/Scripts/MainControllerScript.cs
+++ Ten Hour Shift/Assets/Scripts/MainControllerScript.cs
@@ -8,7 +8,31 @@ public class MainControllerScript : MonoBehaviour
     void Start()
     {
         Globals.mabel.printPatientName();
-        print(Globals.mabel.dialogue[1].hasNextLine());
+        while (Globals.mabel.dialogue[1].hasNextLine())
+        {
+            print(Globals.mabel.dialogue[1].getNextLine());
+        }
+        if (Globals.mabel.dialogue[1].needPlayerInputNow)
+        {
+            print(Globals.mabel.dialogue[1].playerResponses[0]);
+            while (Globals.mabel.dialogue[1].hasNextBranchingLine(1))
+            {
+                print(Globals.mabel.dialogue[1].getBranchingLine(1));
+            }
+            Globals.mabel.dialogue[1].reset();
+            print(Globals.mabel.dialogue[1].playerResponses[1]);
+            while (Globals.mabel.dialogue[1].hasNextBranchingLine(2))
+            {
+                print(Globals.mabel.dialogue[1].getBranchingLine(2));
+            }
+            Globals.mabel.dialogue[1].reset();
+            print(Globals.mabel.dialogue[1].playerResponses[2]);
+            while (Globals.mabel.dialogue[1].hasNextBranchingLine(3))
+            {
+                print(Globals.mabel.dialogue[1].getBranchingLine(3));
+            }
+            
+        }
     }
 
     // Update is called once per frame
