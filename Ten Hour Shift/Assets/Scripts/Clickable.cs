55f8c7881bf1a1a2fdf1e191614caa03c0a2bd14
Ruonan Chen <chennylan@163.com>
2020-01-15 11:09:44 -0800

    add meeting room and breakroom clickable

--- Ten Hour Shift/Assets/Scripts/Clickable.cs
+++ Ten Hour Shift/Assets/Scripts/Clickable.cs
@@ -9,11 +9,12 @@ public class Clickable : MonoBehaviour
     public Vector3 starting_camere_position;
     //public Transform[] rooms;
     // Start is called before the first frame update
-    Vector3 originalEulerAngle, upperRoomEulerAngle, buttomRoomEulerAngle;
+    Vector3 originalEulerAngle, upperRoomEulerAngle, buttomRoomEulerAngle, breakRoomEulerAngle;
     void Start()
     {
         starting_camere_position = m_camera.transform.position;
         originalEulerAngle = m_camera.transform.rotation.eulerAngles;
+        breakRoomEulerAngle = new Vector3(30.0f, 90.0f, 0.0f);
         upperRoomEulerAngle = new Vector3(30.0f, 180.0f, 0.0f);
         buttomRoomEulerAngle = new Vector3(30.0f, 0.0f, 0.0f);
         //Debug.Log("cameraPos" + cameraPos);
@@ -34,26 +35,38 @@ public class Clickable : MonoBehaviour
         if (gameObject.name == "GreenClickable")
         {
             Debug.Log("Move to green");
-            m_camera.transform.position = new Vector3(-37.0f, 7.0f, -29.0f);
+            m_camera.transform.position = new Vector3(-37.0f, 7.0f, -29.0f); //green room cam position
             m_camera.transform.eulerAngles = buttomRoomEulerAngle;
         }
         else if (gameObject.name == "BlueClickable")
         {
             Debug.Log("Move to blue");
-            m_camera.transform.position = new Vector3(-11.0f, 7.0f, -29.0f);
+            m_camera.transform.position = new Vector3(-11.0f, 7.0f, -29.0f); //blue room cam position
             m_camera.transform.eulerAngles = buttomRoomEulerAngle;
         }
         else if (gameObject.name == "GrassClickable")
         {
             Debug.Log("Move to grass");
-            m_camera.transform.position = new Vector3(-11.0f, 7.0f, 33.0f);
+            m_camera.transform.position = new Vector3(-11.0f, 7.0f, 33.0f); //grass room cam position
             m_camera.transform.eulerAngles = upperRoomEulerAngle;
         }
         else if (gameObject.name == "PurpleClickable")
         {
             Debug.Log("Move to purple");
-            m_camera.transform.position = new Vector3(12.0f, 7.0f, 33.0f);
+            m_camera.transform.position = new Vector3(12.0f, 7.0f, 33.0f); //purple room cam position
             m_camera.transform.eulerAngles = upperRoomEulerAngle;
+        }
+        else if (gameObject.name == "BreakroomClickable")
+        {
+            Debug.Log("Move to break room");
+            m_camera.transform.position = new Vector3(-55.0f, 8.0f, 11.0f); //break room cam position
+            m_camera.transform.eulerAngles = breakRoomEulerAngle;
+        }
+        else if (gameObject.name == "MeetingRoomClickable")
+        {
+            Debug.Log("Move to meeting room");
+            m_camera.transform.position = new Vector3(12.0f, 8.0f, -29.0f); //break room cam position
+            m_camera.transform.eulerAngles = buttomRoomEulerAngle;
         }
     }
 }
